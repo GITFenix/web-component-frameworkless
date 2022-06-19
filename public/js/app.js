919615f28d87ae684f257eced783b499074341fd
Tomasz Strzelecki <strzelecki.tomasz@gmail.com>
2022-06-19 22:50:24 +0200

    simple list from provided api items

--- public/js/app.js
+++ public/js/app.js
@@ -1,45 +1,47 @@
+import './component/header.js'
+
 export class TodoApp {
     #title = 'my todo app';
-    #items = ['Nir', 'You', 'JS'];
+    #items = [];
 
     constructor() {
         this.loadItems();
     }
 
     async loadItems() {
-        const items = await  fetch('https://jsonplaceholder.typicode.com/todos').then(response => response.json());
-        this.#items = items.map(item => item.title);
+        this.#items = await fetch('http://localhost').then(response => response.json());
         this.render();
     }
 
     render() {
-        document.body.innerHTML = `
+        let content = `
             <section class="todoapp">
-    
-              <header class="header">
-                <h1>${this.#title}</h1>
-                <input class="new-todo"
-                       placeholder="What needs to be done?"
-                       autofocus />
-              </header>
-            
+              <todo-header title="${this.#title}"></todo-header>
               <section class="main">
                 <input class="toggle-all"
                        type="checkbox" />
+        `;
+
+        this.#items.forEach(function(item) {
+            content += `
                 <ul class="todo-list">
                   <li>
                     <div class="view">
                       <input class="toggle"
                              type="checkbox" />
-                      <label>Todo Title</label>
+                      <label>${item.name}</label>
                       <button class="destroy" />
                     </div>
                     <input class="edit" />
-                  </li>
+                  </li> 
                 </ul>
-              </section>
-        `;
+            `;
+        })
+
+        content += `</section>
+            </section>
+        ;`
+
+        document.body.innerHTML = content;
     }
-
-
 }
